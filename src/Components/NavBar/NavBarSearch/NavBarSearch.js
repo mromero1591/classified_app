0c4266e6f4535cece164216a151801f90b8760b0
Mark Romero <markromero@marks-mbp.lan>
2019-05-08 01:07:39 -0600

    updated speed of search transations

--- src/Components/NavBar/NavBarSearch/NavBarSearch.js
+++ src/Components/NavBar/NavBarSearch/NavBarSearch.js
@@ -3,9 +3,16 @@ import React from 'react';
 import {X} from 'react-feather';
 
 export default function NavBarSearch(props) {
+
+  function submitOnEnter(keyCode) {
+    if(keyCode === 13) {
+      props.updateActiveSearch();
+    }
+  }
+
   return (
     <div className={`navbar-search-section ${props.active ? 'navbar-search-section-active' : ''}`}>
-      <input className='input' type="text" placeholder='Search here...'/>
+      <input onKeyUp={(e) => submitOnEnter(e.keyCode)} className='input' type="text" placeholder='Search here...'/>
       <X onClick={ () => {props.updateActiveSearch()}}/>
     </div>
   )
